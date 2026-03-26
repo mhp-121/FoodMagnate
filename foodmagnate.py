aad76541b48f09f3f6265499fa9fda85f105f8e9
Ncab08 <27noahbelg@kssf.kmat.org.uk>
2026-03-26 09:49:01 +0000

    Update foodmagnate.py
    added hiiiiiiiiiiii comment

--- foodmagnate.py
+++ foodmagnate.py
@@ -2,6 +2,7 @@
 #this code should be used in conjunction with the Preliminary Material
 #written by the AQA Programmer Team
 #developed in the Python 3.5.1 programming environment
+#hiiiiiiiiiiiiiiii
 
 import math
 import random
